c1929bc0ceaec17d1b70c77d3b817e80dc031190
Lorenz A <me@lorenzackermann.xyz>
2020-11-27 00:39:40 +0100

    baseic eslint & pretter setup

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,27 +1,22 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import path from 'path';
 import { Compiler, Configuration, RuleSetUse, WebpackPluginInstance } from 'webpack';
-import ReactRefreshPlugin from '@pmmmwh/react-refresh-webpack-plugin'
-import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin'
+import ReactRefreshPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
-const tsRuleUse: RuleSetUse = [
-  { loader: 'ts-loader', options: { transpileOnly: true },}
-];
+const tsRuleUse: RuleSetUse = [{ loader: 'ts-loader', options: { transpileOnly: true } }];
 
-const plugins: (
-  | ((this: Compiler, compiler: Compiler) => void)
-  | WebpackPluginInstance
-)[] = [
+const plugins: (((this: Compiler, compiler: Compiler) => void) | WebpackPluginInstance)[] = [
   new ForkTsCheckerWebpackPlugin(),
-  new HtmlWebpackPlugin({ template: path.resolve(__dirname, "src", "index.html"), }),
+  new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'index.html') }),
 ];
 
-if (isDevelopment){
+if (isDevelopment) {
   // fast refresh
-  tsRuleUse.unshift({ loader: 'babel-loader', options: { plugins: ['react-refresh/babel'] },})
-  plugins.unshift(new ReactRefreshPlugin())
+  tsRuleUse.unshift({ loader: 'babel-loader', options: { plugins: ['react-refresh/babel'] } });
+  plugins.unshift(new ReactRefreshPlugin());
 }
 
 const config: Configuration = {
@@ -29,25 +24,24 @@ const config: Configuration = {
   entry: {
     main: './src/index.tsx',
   },
-  target: "web",
+  target: 'web',
   output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
   },
   resolve: {
-    extensions: [".js", ".ts", ".tsx"],
+    extensions: ['.js', '.ts', '.tsx'],
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         include: path.join(__dirname, 'src'),
-        use: tsRuleUse
+        use: tsRuleUse,
       },
     ],
   },
   plugins,
 };
-
 
 export default config;
